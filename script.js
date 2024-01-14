3c529fb0ad06803491dd08be15ac81686adc796a
Léo Riché <115212297+Leo-Riche@users.noreply.github.com>
2024-01-14 21:21:20 +0100

    ✅ Update script.js

--- script.js
+++ script.js
@@ -150,7 +150,7 @@ button.addEventListener('click', () => {
     
 });
 
-fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+fetch('https://pokeapi.co/api/v2/pokemon?limit=1302')
     .then((response) => response.json())
     .then((data) => {
     data.results.forEach((pokemon) => {
